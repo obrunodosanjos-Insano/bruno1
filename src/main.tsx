aff936b4b8c25091fac37aa61c7d87c818768552
Bruno <obrunodosanjos@gmail.com>
2026-09-02 14:45:31 -0300

    feat: ativar modo visitante opcional

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import GuestMode from './GuestMode.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GuestMode />
   </StrictMode>,
 );
